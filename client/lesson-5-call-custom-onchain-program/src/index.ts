044edfa80d92461a6c4e0680d763c597a00be803
Jian Hong <77224948+jianhong1998@users.noreply.github.com>
2024-11-24 20:55:57 +0800

    remove unused import from lesson 5

--- client/lesson-5-call-custom-onchain-program/src/index.ts
+++ client/lesson-5-call-custom-onchain-program/src/index.ts
@@ -1,10 +1,5 @@
 import { SolanaDevConnection } from './connection/solana-dev-connection';
-import {
-  AirdropUtil,
-  BalanceUtil,
-  PingUtil,
-  TransactionUtil,
-} from './utils/transaction';
+import { AirdropUtil, BalanceUtil, PingUtil } from './utils/transaction';
 import { SENDER_KEYPAIR } from './constants';
 
 const main = async () => {
