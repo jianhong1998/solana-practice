304f8e5319d4b3bc343d7980b2e9be605473c504
Jian Hong <77224948+jianhong1998@users.noreply.github.com>
2024-12-26 20:32:56 +0800

    complete project 7

--- projects/07-swap-program/programs/swap-program/src/lib.rs
+++ projects/07-swap-program/programs/swap-program/src/lib.rs
@@ -13,9 +13,22 @@ declare_id!("HE1mqZFn3DAuNbj1YL3MfRqEWeZ8uUMWBgcAV6J9PzJb");
 
 #[program]
 pub mod swap_program {
-    use super::*;
+  use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        initialize::handler(ctx)
-    }
+  pub fn make_offer(
+    context: Context<MakeOffer>,
+    id: u64,
+    token_a_offered_amount: u64,
+    token_b_wanted_amount: u64,
+  ) -> Result<()> {
+    instructions::make_offer::send_offered_token_to_vault(&context, token_a_offered_amount)?;
+    instructions::save_offer(context, id, token_b_wanted_amount)
+  }
+
+  pub fn take_offer(context: Context<TakeOffer>) -> Result<()> {
+    instructions::take_offer::send_wanted_token_to_maker(&context)?;
+    instructions::take_offer::withdraw_and_close_vault(context)?;
+
+    Ok(())
+  }
 }
