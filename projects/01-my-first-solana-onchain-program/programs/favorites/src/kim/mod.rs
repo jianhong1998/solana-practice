3f4c1e7c43249c86a6f3650697689a598c9a32c6
Jian Hong <77224948+jianhong1998@users.noreply.github.com>
2025-01-26 18:13:54 +0800

    add kim's code into project-1

---
+++ projects/01-my-first-solana-onchain-program/programs/favorites/src/kim/mod.rs
@@ -0,0 +1,3 @@
+mod favourite;
+
+pub use favourite::*;
---
+++ projects/01-my-first-solana-onchain-program/tests/anchor.spec.ts
@@ -0,0 +1,51 @@
+import * as anchor from '@coral-xyz/anchor';
+import { Keypair, Transaction } from '@solana/web3.js';
+import BN from 'bn.js';
+import IDL from '../target/idl/favorites.json';
+import { Favorites } from '../target/types/favorites';
+import signerKeyString from './fixtures/wallets/signer.json';
+
+describe('Test', () => {
+  let program: anchor.Program<Favorites>;
+  let userKeypair: Keypair;
+
+  beforeAll(() => {
+    const provider = anchor.AnchorProvider.env();
+    anchor.setProvider(provider);
+    program = anchor.workspace['favorites'] as anchor.Program<Favorites>;
+
+    userKeypair = Keypair.fromSecretKey(
+      Uint8Array.from(signerKeyString as number[])
+    );
+  });
+
+  it('should be able to execute', async () => {
+    // const instruction = await program.methods
+    //   .setFavouritesKim(new BN(1), 'Blue', ['coding'])
+    //   .accounts({
+    //     user: userKeypair.publicKey,
+    //   })
+    //   .signers([userKeypair])
+    //   .instruction();
+
+    // const blockhashObj = await program.provider.connection.getLatestBlockhash();
+
+    // const transaction = new Transaction({
+    //   blockhash: blockhashObj.blockhash,
+    //   lastValidBlockHeight: blockhashObj.lastValidBlockHeight,
+    // }).add(instruction);
+
+    // const transactionId = await program.provider.connection.sendTransaction(
+    //   transaction,
+    //   [userKeypair]
+    // );
+
+    const transactionId = await program.methods
+      .setFavouritesKim(new BN(1), 'blue', ['coding'])
+      .accounts({ user: userKeypair.publicKey })
+      .signers([userKeypair])
+      .rpc();
+
+    console.log({ transactionId });
+  });
+});
